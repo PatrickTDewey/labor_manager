6c357f1e8c2e44a1587f55c0cde05600f97b8159
SpencerRauch <rauchs@gmail.com>
2021-09-21 13:30:58 -0700

    minor changes we zoomed on

--- client/src/components/EditWorkerForm.jsx
+++ client/src/components/EditWorkerForm.jsx
@@ -38,54 +38,54 @@ const EditWorkerForm = (props) => {
     }
     return (
         <>
-        { worker ?
-        <form onSubmit={onSubmitHandler} >
-            <div className="form-floating mb-3">
-                <input type="text" className="form-control" name="firstName" id="firstName" placeholder="first name" value={worker.firstName} onChange={changeHandler} />
-                <label htmlFor="firstName">First Name:</label>
-            </div>
-            <div className="form-floating mb-3">
-                <input type="text" className="form-control" name="lastName" id="lastName" placeholder="last name" value={worker.lastName} onChange={changeHandler} />
-                <label htmlFor="lastName">Last Name:</label>
-            </div>
-            {/* <div className="form-floating mb-3">
+            {worker ?
+                <form onSubmit={onSubmitHandler} >
+                    <div className="form-floating mb-3">
+                        <input type="text" className="form-control" name="firstName" id="firstName" placeholder="first name" value={worker.firstName} onChange={changeHandler} />
+                        <label htmlFor="firstName">First Name:</label>
+                    </div>
+                    <div className="form-floating mb-3">
+                        <input type="text" className="form-control" name="lastName" id="lastName" placeholder="last name" value={worker.lastName} onChange={changeHandler} />
+                        <label htmlFor="lastName">Last Name:</label>
+                    </div>
+                    {/* <div className="form-floating mb-3">
                 <input type="text" className="form-control" name="position" id="position" placeholder="position" value={form.position} onChange={(e) => setForm({ ...form, position: e.target.value })} />
                 <label htmlFor="position">Position:</label>
             </div> */}
-                   <div>
-                   <label htmlFor='0'>Monday</label>
-                   <input type='checkbox' name='0' checked={worker.availability[0]} onChange={onChangeBox}/>
-                   </div>
-                   <div>
-                   <label htmlFor='1'>Tuesday</label>
-                   <input type='checkbox' name='1' checked={worker.availability[1]} onChange={onChangeBox}/>
-                   </div>
-                   <div>
-                   <label htmlFor='2'>Wednesday</label>
-                   <input type='checkbox' name='2' checked={worker.availability[2]} onChange={onChangeBox}/>
-                   </div>
-                   <div>
-                   <label htmlFor='3'>Thursday</label>
-                   <input type='checkbox' name='3' checked={worker.availability[3]} onChange={onChangeBox}/>
-                   </div>
-                   <div>
-                   <label htmlFor='4'>Friday</label>
-                   <input type='checkbox' name='4' checked={worker.availability[4]} onChange={onChangeBox}/>
-                   </div>
-                   <div>
-                   <label htmlFor='5'>Saturday</label>
-                   <input type='checkbox' name='5' checked={worker.availability[5]} onChange={onChangeBox}/>
-                   </div>
-                   <label htmlFor='6'>Sunday</label>
-                   <input type='checkbox' name='6' checked={worker.availability[6]} onChange={onChangeBox}/>
-                   <div>
-                   </div>
-            <input type="submit" value="Edit" className="btn btn-primary" />
-            <Link className="ms-3" to='/'>Home</Link>
-        </form> 
-                   
-        : null
-        }
+                    <div>
+                        <label htmlFor='0'>Monday</label>
+                        <input type='checkbox' name='0' checked={worker.availability[0]} onChange={onChangeBox} />
+                    </div>
+                    <div>
+                        <label htmlFor='1'>Tuesday</label>
+                        <input type='checkbox' name='1' checked={worker.availability[1]} onChange={onChangeBox} />
+                    </div>
+                    <div>
+                        <label htmlFor='2'>Wednesday</label>
+                        <input type='checkbox' name='2' checked={worker.availability[2]} onChange={onChangeBox} />
+                    </div>
+                    <div>
+                        <label htmlFor='3'>Thursday</label>
+                        <input type='checkbox' name='3' checked={worker.availability[3]} onChange={onChangeBox} />
+                    </div>
+                    <div>
+                        <label htmlFor='4'>Friday</label>
+                        <input type='checkbox' name='4' checked={worker.availability[4]} onChange={onChangeBox} />
+                    </div>
+                    <div>
+                        <label htmlFor='5'>Saturday</label>
+                        <input type='checkbox' name='5' checked={worker.availability[5]} onChange={onChangeBox} />
+                    </div>
+                    <div>
+                    <label htmlFor='6'>Sunday</label>
+                    <input type='checkbox' name='6' checked={worker.availability[6]} onChange={onChangeBox} />
+                    </div>
+                    <input type="submit" value="Edit" className="btn btn-primary" />
+                    <Link className="ms-3" to='/'>Home</Link>
+                </form>
+
+                : null
+            }
         </>
     )
 }
