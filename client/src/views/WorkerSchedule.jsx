cad58b5c7beb42e9d988e59e0b518df40fae34bb
SpencerRauch <rauchs@gmail.com>
2021-09-22 14:48:09 -0700

    formatting of code

--- client/src/views/WorkerSchedule.jsx
+++ client/src/views/WorkerSchedule.jsx
@@ -42,24 +42,22 @@ const WorkerSchedule = () => {
     const [unavailable, setUnavailable] = useState([])
     const [time, setTime] = useState([])
     const [select, setSelect] = useState('24')
-    const sortFunc = (a,b) => {
+    const sortFunc = (a, b) => {
         let sortNum = a.lastName.localeCompare(b.lastName)
-        if (sortNum !== 0){
+        if (sortNum !== 0) {
             return sortNum
         }
-        if (a._id < b._id){
+        if (a._id < b._id) {
             return -1
         }
         return 1;
-
-
     }
     useEffect(() => {
         axios.get("http://localhost:8000/api/workers")
             .then(res => {
                 let unsorted = res.data
                 // availability
-                setWorkers(unsorted.filter(worker => worker.availability[day_id - 1]).sort((a,b) => sortFunc(a,b)))
+                setWorkers(unsorted.filter(worker => worker.availability[day_id - 1]).sort((a, b) => sortFunc(a, b)))
                 setUnavailable(unsorted.filter(worker => !(worker.availability[day_id - 1])))
                 setTime(time.length > 1 ? time : Object.keys(res.data[0].working));
             })
@@ -76,7 +74,7 @@ const WorkerSchedule = () => {
         axios.put('http://localhost:8000/api/workers/update/' + worker._id, worker)
             .then(res => {
                 let unsorted = [...filteredWorkers, worker]
-                setWorkers(unsorted.sort((a, b) => sortFunc(a,b)))
+                setWorkers(unsorted.sort((a, b) => sortFunc(a, b)))
             })
             .catch(err => console.log(err))
     }
@@ -85,13 +83,13 @@ const WorkerSchedule = () => {
         setSelect(e.target.value)
         let timeCopy = [...time]
         if (e.target.value === '12') {
-            setTime(timeCopy.map(time => ((parseInt(time)+ 11) % 12 + 1) + time.slice(time.indexOf(':')) + (parseInt(time) >= 12 ? ' PM': ' AM')))
+            setTime(timeCopy.map(time => ((parseInt(time) + 11) % 12 + 1) + time.slice(time.indexOf(':')) + (parseInt(time) >= 12 ? ' PM' : ' AM')))
         } else {
             setTime(timeCopy.map(time => {
                 const [timeStr, amPm] = time.split(' ');
                 let [hours, minutes] = timeStr.split(':');
                 if (amPm === 'AM' && hours.length === 1) {
-                    hours = '0'+ hours;
+                    hours = '0' + hours;
                 }
                 if (parseInt(hours) === 12) {
                     hours = '00'
@@ -135,18 +133,18 @@ const WorkerSchedule = () => {
                                 {workers && workers.map((worker, i) => {
                                     return <tr key={i}>
                                         <td>{worker.firstName} {worker.lastName} </td>
-                                        {Object.keys(worker.working).map((key, idx) => <td key={idx}><button onClick={(e) => { handleClick(e, worker, key) }} style={{width: '100%'}}className={parseInt(worker.working[key][day_id - 1]) === 0 ? 'btn btn-danger' : parseInt(worker.working[key][day_id - 1]) === 1 ? 'btn btn-primary' : 'btn btn-warning'} value={1}></button></td>)}
+                                        {Object.keys(worker.working).map((key, idx) => <td key={idx}><button onClick={(e) => { handleClick(e, worker, key) }} style={{ width: '100%' }} className={parseInt(worker.working[key][day_id - 1]) === 0 ? 'btn btn-danger' : parseInt(worker.working[key][day_id - 1]) === 1 ? 'btn btn-primary' : 'btn btn-warning'} value={1}></button></td>)}
                                     </tr>
                                 })}
                             </tbody>
                         </table>
                     </div>
                     <div className="legend d-flex align-items-center justify-content-center">
-                        <span className='btn btn-primary me-2' style={{'cursor':'auto'}} ></span>
+                        <span className='btn btn-primary me-2' style={{ 'cursor': 'auto' }} ></span>
                         <span className="me-2">Working</span>
-                        <span className='btn btn-danger me-2' style={{'cursor':'auto'}}></span>
+                        <span className='btn btn-danger me-2' style={{ 'cursor': 'auto' }}></span>
                         <span className="me-2">Not scheduled</span>
-                        <span className='btn btn-warning me-2' style={{'cursor':'auto'}}></span>
+                        <span className='btn btn-warning me-2' style={{ 'cursor': 'auto' }}></span>
                         <span>Lunch</span>
                     </div>
                     {unavailable.length >= 1 ?
