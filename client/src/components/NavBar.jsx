df26711feae8fb2e352e9fbcb741f83836cb7c79
SpencerRauch <rauchs@gmail.com>
2021-09-21 17:46:40 -0700

    reordered links

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom'
 const NavBar = () => {
     return (
         <div>
-            <Link to='/workers/add' className="btn btn-link">Add Worker</Link> 
+            <Link to='/' className="ms-2 btn btn-link">Home</Link>
             <Link to='/schedule/day/1' className="ms-2 btn btn-link">View Schedule</Link>
             <Link to='/adjust_hours' className="ms-2 btn btn-link">Adjust Hours</Link>
-            <Link to='/' className="ms-2 btn btn-link">Home</Link>
+            <Link to='/workers/add' className="btn btn-link">Add Worker</Link> 
         </div>
     )
 }
